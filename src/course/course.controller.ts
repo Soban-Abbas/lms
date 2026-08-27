54079e699d5671f1041c360267d7ac3ea5c4bf07
Soban Abbas <sobanabbass@gmail.com>
2026-08-27 18:35:31 +0500

    implemeting pagination

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,4 +1,4 @@
-import { Controller,Post,UseGuards,Body, Req } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body, Req,Get, Query } from '@nestjs/common';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
@@ -6,11 +6,18 @@ import { CourseService } from './course.service';
 
 @Controller('course')
 export class CourseController {
-    constructor(private courseService : CourseService ){}
+    constructor(private courseService: CourseService) { }
     @Post()
-    @UseGuards(JwtAuthGuard,RolesGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Roles('teacher')
-    registerCourse(@Body()  body :{ title:string , description:string  },@Req() req ){
-return this.courseService.registerCourse(body.title, body.description,req.user.id)
+    registerCourse(@Body() body: { title: string, description: string }, @Req() req) {
+        return this.courseService.registerCourse(body.title, body.description, req.user.id)
+    }
+
+
+
+    @Get()
+    getCourses(@Query() query :any){
+        return this.courseService.getCourse(query)
     }
 }
--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Course } from './course.entity';
-import { Repository } from 'typeorm';
-import { promises } from 'dns';
+import { Repository, TreeRepository } from 'typeorm';
 
 @Injectable()
 export class CourseService {
@@ -19,4 +18,36 @@ export class CourseService {
 const course = this.courseRepository.create({title , description,teachers:[{id:teacherId} as any]});
 return this.courseRepository.save(course)
     }
+
+
+
+    async getCourse(query:any):Promise<Course[]>{
+        
+        const page :string = query.page? query.page:'1'
+        const items : string = query.items? query.items:'5';
+        const offset=(Number(page)-1)*Number (items)
+        const data = await this.courseRepository.find({
+            relations:{
+                teachers:true
+            },
+            select:{
+                id:true,
+                title:true,
+                description:true,
+                createdAt:true,
+                teachers:{
+                    name:true,
+                    email:true
+                }
+            }
+            ,
+            
+        })
+       
+        return data
+        
+        
+    }
+
+
 }
